8c81c98b4813dd0465b5b80275001458b8e017d3
Jennifer <jenniferguay01@gmail.com>
2022-11-29 10:02:55 +0800

    commit inital just exploring

---
+++ Rforeveryone.R
@@ -0,0 +1,35 @@
+1+1
+
+44+5+6
+
+59+24*2/4
+
+Remember it always follow PEMDAS IN ARITHMETIC 
+
+(23*3)+12/2
+
+LESSON 2:VARIABLES
+
+x < -3
+
+x = 4
+
+x <- y <- 4
+
+age_boys
+
+assign("z", "love")
+
+k <-C(TRUE, FALSE)
+
+5 1=10/2
+
+x <-1:20 #assingning x values form 1 to 20
+
+
+x
+
+1:20
+
+
+
